fa4ae7a3198dadf16a6b4b112389b53895e1674b
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-03-06 16:22:39 +0000

    Update for application. Comment out like 26 and connection works well.

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
 
-// listen
-app.listen(3005, () => {
-    console.log('Server listening on port 3005.');
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database');
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// routes setup
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    // something going here.
+})
+
+app.use('/admin', adminRoutes);
+
+mongoConnect(client => {
+    console.log(client);
+    app.listen(3000);
 })
